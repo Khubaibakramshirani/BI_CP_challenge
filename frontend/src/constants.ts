141e923822491e9a401b49b03be465ac8b9ae0e8
Khubaibakramshirani <147776680+Khubaibakramshirani@users.noreply.github.com>
2025-02-17 10:14:40 +0000

    updated file

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,5 @@
 // constants.ts
 
 // Base URLs
-export const BASE_API_URL = 'http://127.0.0.1:8000/api'
-//'https://bounceinsights-coding-challenge.onrender.com/api';
+export const BASE_API_URL = 'https://bi-cp-challenge.onrender.com/api';
 //'http://127.0.0.1:8000/api'//
