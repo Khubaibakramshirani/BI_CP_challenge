a693dff398fb6f94830f5889831215d1df7867e0
Khubaibakramshirani <147776680+Khubaibakramshirani@users.noreply.github.com>
2025-02-18 17:23:33 +0000

    docker-combined.yml file and updated main.py CORSMiddleware allow_origins=  * and constant.ts

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,7 @@ app = FastAPI()
 # Add CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000"#,  # Allow local development
-        #"https://bounceinsights-coding-challenge.onrender.com"  # Replace with your actual Render URL
-    ],
+    allow_origins=["*"], #"http://localhost:3000"#,  # Allow local development   #"https://bounceinsights-coding-challenge.onrender.com"  # Replace with your actual Render URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
